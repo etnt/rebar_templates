06e52bc77eac22cb7a1c693b0619ae6abd0078db
Torbjorn Tornkvist <tobbe@klarna.com>
2010-10-06 10:46:13 +0200

    Adding some more jqt stuff to sample page.

--- mobile_static_file.erl
+++ mobile_static_file.erl
@@ -34,19 +34,11 @@ figure_out_content_type(Name) ->
         _     -> what_content_type(undefined)
     end.
 
-
-what_content_type("png")    -> "image/png";
-what_content_type("jpg")    -> "image/jpeg";
-what_content_type("jpeg")   -> "image/jpeg";
-what_content_type("gif")    -> "image/gif";
-what_content_type("mp3")    -> "audio/mpeg";
-what_content_type("mpeg")   -> "video/mpeg";
-what_content_type("avi")    -> "video/x-msvideo";
-what_content_type("txt")    -> "text/plain";
-what_content_type("xml")    -> "text/xml";
-what_content_type("html")   -> "text/html";
-what_content_type("css")    -> "text/css";
-what_content_type("csv")    -> "text/x-csvpp";
-what_content_type("pdf")    -> "application/pdf";
-what_content_type("ps")     -> "application/postscript";
-what_content_type(_)        -> "application/octet-stream".
+what_content_type(Suffix) -> 
+    try 
+        {_Type, ContentType} = mime_types:t(Suffix),
+        ContentType
+    catch
+        _:_ ->
+            "application/octet-stream"
+    end.
